edf127656f5df5c0c216d1b2c472338f5820e170
hacker <18368116334@163.com>
2018-12-14 10:38:02 +0800

    LocalDateTime 测试

--- Test/src/main/java/com/hacker/demo/LocalDateTimeTest.java
+++ Test/src/main/java/com/hacker/demo/LocalDateTimeTest.java
@@ -16,6 +16,8 @@ import java.time.temporal.TemporalAdjusters;
  */
 public class LocalDateTimeTest {
 
+    private static final Integer DAY_MILL = 24 * 60 * 60 * 1000;
+
     @Test
     public void test() {
         Long start = LocalDateTime.of(LocalDate.now(), LocalTime.MIN)
@@ -24,32 +26,26 @@ public class LocalDateTimeTest {
         System.out.println(start);
     }
 
-    private static final Integer DAY_MILL = 24 * 60 * 60 * 1000;
+
+    /**
+     * 获取上个月的开始时间戳
+     */
+    @Test
+    public void testLocalDateTime(){
+        //指定到当前时间的零点
+        LocalDateTime of = LocalDateTime.of(LocalDate.now(), LocalTime.MIN);
+        //指定到本月1号零点
+        LocalDateTime with = of.with(TemporalAdjusters.firstDayOfMonth());
+        //指定到上个月
+        LocalDateTime localDateTime = with.minusMonths(1L);
+        //调整时差，转换成时间戳
+        long lastMonthBegin = localDateTime.toInstant(ZoneOffset.ofHours(8)).toEpochMilli();
+        System.out.println(lastMonthBegin);
+    }
+
+
 
     public static void main(String[] args) {
-
-//        LocalDateTime today_start = LocalDateTime.of(LocalDate.now(), LocalTime.MIN);
-//        long start = today_start.toInstant(ZoneOffset.ofHours(8)).toEpochMilli(); //今日开始时间
-//
-//        LocalDateTime today_end = LocalDateTime.of(LocalDate.now(), LocalTime.MAX);
-//        long end = today_end.toInstant(ZoneOffset.ofHours(8)).toEpochMilli(); //今日结束时间
-//
-        //这种方式很够，每个月1号都有问题，因为变成了0,..
-//        int year = today_start.getYear();
-//        Month month = today_start.getMonth();
-//        int day = today_start.getDayOfMonth() - 1;
-//        LocalDateTime yesterday_start = LocalDateTime.of(year, month, day, 0, 0, 0);
-//        long yesterday_start_time = yesterday_start.toInstant(ZoneOffset.ofHours(8)).toEpochMilli();
-//
-//        LocalDateTime yesterday_end = LocalDateTime.of(today_start.getYear(), today_start.getMonth(), today_start
-//                .getDayOfMonth() - 1, 23, 59, 59);
-//        long yesterday_end_time = yesterday_end.toInstant(ZoneOffset.ofHours(8)).toEpochMilli();
-//
-//        System.out.println("start " + start);
-//        System.out.println("end " + end);
-//        System.out.println("yesterday_start_time " + yesterday_start_time);
-//        System.out.println("yesterday_end_time " + yesterday_end_time);
-
 
         LocalDateTime today_start = LocalDateTime.of(LocalDate.now(), LocalTime.MIN);
         long start = today_start.toInstant(ZoneOffset.ofHours(8)).toEpochMilli(); //今日开始时间
@@ -61,6 +57,5 @@ public class LocalDateTimeTest {
         long _start = start - DAY_MILL;
         long _end = end - DAY_MILL;
         System.out.println(_start + " --- " + _end);
-
     }
 }
